161b174fc6a2645ac041113e132e76108487a242
boni <boni619@gmail.com>
2020-02-11 23:57:27 +0530

    Updates

---
+++ precache-manifest.ec0dc32fb12f757a3aa8989c176a8a3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1da5b6dc9cb93b904aa0dfd5e6dfe3",
+    "url": "/monsters-rolodex-new/index.html"
+  },
+  {
+    "revision": "5eb350481fc1b69a3e04",
+    "url": "/monsters-rolodex-new/static/css/main.3d9dd07c.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex-new/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex-new/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5eb350481fc1b69a3e04",
+    "url": "/monsters-rolodex-new/static/js/main.0ebb0467.chunk.js"
+  },
+  {
+    "revision": "82f69b38872f85308541",
+    "url": "/monsters-rolodex-new/static/js/runtime-main.4a5a37f3.js"
+  }
+]);
